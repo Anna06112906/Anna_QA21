d19683e25b547b2c04f9158360b58bdb6e952e60
Anna06112906 <annabalabuha77@gmail.com>
2019-10-24 11:02:50 +0300

    Added log, listener, screenshot

--- wiki-web-tests/src/test/java/com/wiki/fw/ApplicationManager.java
+++ wiki-web-tests/src/test/java/com/wiki/fw/ApplicationManager.java
@@ -1,17 +1,44 @@
 package com.wiki.fw;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager{
-    WebDriver driver;
+   EventFiringWebDriver driver;
     ArticleHelper article;
     SessionHelper session;
     private String browser;
+
+    public static class MyListener extends AbstractWebDriverEventListener {
+        Logger logger = LoggerFactory.getLogger(MyListener.class);
+
+       /* @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            logger.info("Start search element " + by);
+        }*/
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+           logger.info(by + " found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            logger.error("error", throwable);
+
+
+        }
+    }
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -19,10 +46,12 @@ public class ApplicationManager{
 
     public void init() {
         if(browser.equals(BrowserType.CHROME)){
-            driver = new ChromeDriver();
+            driver = new EventFiringWebDriver(new ChromeDriver());
         }if(browser.equals(BrowserType.FIREFOX)){
-            driver = new FirefoxDriver();
+            driver = new EventFiringWebDriver(new FirefoxDriver()) ;
         }
+
+        driver.register(new MyListener());
 
         //add default wait for WebElement
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
--- wiki-web-tests/src/test/java/com/wiki/tests/SearchTests.java
+++ wiki-web-tests/src/test/java/com/wiki/tests/SearchTests.java
@@ -3,6 +3,8 @@ package com.wiki.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class SearchTests extends TestBase {
 
 
@@ -15,11 +17,12 @@ public class SearchTests extends TestBase {
     }
 
     @Test(priority = 1)
-    public void searchWikiTestQA() {
+    public void searchWikiTestQA() throws IOException {
         app.getArticle().typeRequest("QA");
         app.getArticle().clickGoButton();
         Assert.assertEquals(app.getArticle().getArticleTitle().toLowerCase(), "QA".toLowerCase());
         //pause
+        app.getArticle().takeScreenshot();
     }
 
 }
